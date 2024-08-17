7c120da012b6267b26d9cb24595787b39b19604c
mateo9856 <mateo9656@gmail.com>
2024-08-17 14:30:12 +0200

    First script, remove from downloads folder

---
+++ scripts/Remove-Downloads.ps1
@@ -0,0 +1,18 @@
+<#
+.DESCRIPTION
+    This script removes Download files.
+    Additionaly you can exclude files/folders by param
+#>
+function Remove-Downloads {
+    param(
+        [string]$Path = "", 
+        [string[]]$ExcludedFolders)
+        [int]$MaxDays = 90
+
+    if ($Path -eq "") {
+        $Path = $env:USERPROFILE + "\Downloads"
+    }
+
+    $daysInterval = $MaxDays * (-1)
+    Get-ChildItem -Path $env:USERPROFILE\Downloads | Where-Object { $_.LastWriteTime -le (Get-Date).AddDays($daysInterval) } | Remove-Item -Force
+}
